67f007bd641faf4dace972a940e7b179b82d1c60
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-12-18 06:22:54 -0500

    #11 naming the NATS client for metrics

--- src/Program.cs
+++ src/Program.cs
@@ -27,6 +27,7 @@ namespace openrmf_msg_compliance
             Options opts = ConnectionFactory.GetDefaultOptions();
             opts.MaxReconnect = -1;
             opts.ReconnectWait = 1000;
+            opts.Name = "openrmf-msg-compliance";
             opts.Url = Environment.GetEnvironmentVariable("NATSSERVERURL");
             opts.AsyncErrorEventHandler += (sender, events) =>
             {
